2189ba32f41518a4402bde6c921850fd174ffb15
rasikakulasinghe <rasikakulasinghe@gmail.com>
2025-10-03 18:50:39 +0530

    feat: integrate NativeWind for styling, add global CSS file, and update configuration

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }]
     ],
     plugins: [
-      "nativewind/babel",
       "react-native-reanimated/plugin",
     ],
   };
--- metro.config.js
+++ metro.config.js
@@ -1,25 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
+  // Enable CSS support for NativeWind
   isCSSEnabled: true,
 });
-
-// Add resolver to handle nativewind on web
-config.resolver = {
-  ...config.resolver,
-  resolveRequest: (context, moduleName, platform) => {
-    // Redirect nativewind JSX runtime to react on web
-    if (platform === 'web') {
-      if (moduleName === 'nativewind/jsx-dev-runtime') {
-        return context.resolveRequest(context, 'react/jsx-dev-runtime', platform);
-      }
-      if (moduleName === 'nativewind/jsx-runtime') {
-        return context.resolveRequest(context, 'react/jsx-runtime', platform);
-      }
-    }
-    return context.resolveRequest(context, moduleName, platform);
-  },
-};
 
 module.exports = config;
